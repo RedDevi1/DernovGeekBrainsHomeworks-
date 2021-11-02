970c5a3985e44ddfa93fb3f50dbb12a649cf72ba
Nikita <nickita.sevsk@gmail.com>
2021-11-02 22:26:49 +0300

    Update Program.cs

--- Lesson_2/Exercise_1/Exercise_1/Program.cs
+++ Lesson_2/Exercise_1/Exercise_1/Program.cs
@@ -4,50 +4,40 @@ namespace Exercise_1
 {
     class Program
     {
-        enum AllLetters
-        {
-            I = 1,
-            V = 5,
-            X = 10,
-            L = 50,
-            C = 100,
-            D = 500,
-            M = 1000
-        }
         static void Main(string[] args)
         {
             string setValue = Console.ReadLine();
             var curNum = 0; // current number
-            AllLetters[] enumArray = new AllLetters[setValue.Length]; // array of enums by the set value
+            int[] numArray = new int[setValue.Length]; // array of arabic numerals by the set value
             for (int i = 0; i < setValue.Length; i++)
             {
                 switch (setValue[i])
                 {
-                    case 'I': enumArray[i] = AllLetters.I; break;
-                    case 'V': enumArray[i] = AllLetters.V; break;
-                    case 'X': enumArray[i] = AllLetters.X; break;
-                    case 'L': enumArray[i] = AllLetters.L; break;
-                    case 'C': enumArray[i] = AllLetters.C; break;
-                    case 'D': enumArray[i] = AllLetters.D; break;
-                    case 'M': enumArray[i] = AllLetters.M; break;
+                    case 'I': numArray[i] = 1; break;
+                    case 'V': numArray[i] = 5; break;
+                    case 'X': numArray[i] = 10; break;
+                    case 'L': numArray[i] = 50; break;
+                    case 'C': numArray[i] = 100; break;
+                    case 'D': numArray[i] = 500; break;
+                    case 'M': numArray[i] = 1000; break;
                 }
             }
             int j = 0;
-            while (j < enumArray.Length - 1)
+            while (j < numArray.Length - 1)
             {
-                if (enumArray[j] >= enumArray[j + 1])
+                if (numArray[j] >= numArray[j + 1])
                 {
-                    curNum += (int)enumArray[j];
+                    curNum += numArray[j];
                     j++;
                 }
                 else
                 {
-                    curNum += ((int)enumArray[j + 1] - (int)enumArray[j]);
+                    curNum += (numArray[j + 1] - numArray[j]);
                     j += 2;
                 }
             }
-            if (j != enumArray.Length)
-                curNum += (int)enumArray[enumArray.Length - 1];
+            if (j != numArray.Length)
+                curNum += numArray[numArray.Length - 1];
 
             Console.Write(curNum);
 
