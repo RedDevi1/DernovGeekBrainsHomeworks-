0be73bfab456924cf8758dca29c95cf5b544936b
Nikita <nickita.sevsk@gmail.com>
2021-11-02 23:36:56 +0300

    Update Exercise_3 by the 5*

--- Lesson_2/Exercise_3/Exercise_3/Program.cs
+++ Lesson_2/Exercise_3/Exercise_3/Program.cs
@@ -12,10 +12,24 @@ namespace Exercise_3
                 Console.WriteLine("Некорректно введен номер. Порядковый номер ткущего месяца должен быть от 1 до 12");
             else
             {
+                Console.WriteLine("Введите минимальную температуру за сутки");
+                var minTemp = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите максимальную температуру за сутки");
+                var maxTemp = Convert.ToInt32(Console.ReadLine());
                 switch (numOfMonth)
                 {
-                    case 1: Console.Write("Текущий месяц - январь"); break;
-                    case 2: Console.Write("Текущий месяц - февраль"); break;
+                    case 1: 
+                        if ((double)(minTemp + maxTemp)/2 > 0)
+                        {
+                            Console.Write("Текущий месяц - январь. Дождливая зима"); break;
+                        }                     
+                        Console.Write("Текущий месяц - январь"); break;                        
+                    case 2:
+                        if ((double)(minTemp + maxTemp) / 2 > 0)
+                        {
+                            Console.Write("Текущий месяц - февраль. Дождливая зима"); break;
+                        }
+                            Console.Write("Текущий месяц - февраль"); break;
                     case 3: Console.Write("Текущий месяц - март"); break;
                     case 4: Console.Write("Текущий месяц - апрель"); break;
                     case 5: Console.Write("Текущий месяц - май"); break;
@@ -25,7 +39,12 @@ namespace Exercise_3
                     case 9: Console.Write("Текущий месяц - сентябрь"); break;
                     case 10: Console.Write("Текущий месяц - октябрь"); break;
                     case 11: Console.Write("Текущий месяц - ноябрь"); break;
-                    case 12: Console.Write("Текущий месяц - декабрь"); break;
+                    case 12:
+                        if ((double)(minTemp + maxTemp) / 2 > 0)
+                        {
+                            Console.Write("Текущий месяц - декабрь. Дождливая зима"); break;
+                        }
+                            Console.Write("Текущий месяц - декабрь"); break;
                 }
             }
 
