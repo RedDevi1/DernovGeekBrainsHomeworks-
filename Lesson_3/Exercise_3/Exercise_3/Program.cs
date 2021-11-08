0cb49aced8609bc35a1f46427321143d06faf20a
Nikita <nickita.sevsk@gmail.com>
2021-11-08 16:23:30 +0300

    Exercise_3

---
+++ Lesson_3/Exercise_3/Exercise_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string s = Console.ReadLine();     
+            for (int index = s.Length - 1; index >= 0; index--)
+                Console.Write(s[index]);
+        }
+    }
+}
