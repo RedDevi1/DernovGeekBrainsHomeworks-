1e904c3788460ec4313aa257de0cb244debce83d
Nikita <nickita.sevsk@gmail.com>
2021-11-08 16:23:03 +0300

    Exercise_1

---
+++ Lesson_3/Exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,]array = new int[5, 5];
+            for (int i = 0; i < array.GetLength(0);i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j)
+                        Console.WriteLine(array[i,j]);
+                    else
+                        continue;
+                }
+            }
+
+        }
+    }
+}
