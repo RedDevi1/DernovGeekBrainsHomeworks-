a631b2ec1d152e44c02019376a2fdcd7ed4a9697
Nikita <nickita.sevsk@gmail.com>
2021-11-08 16:23:44 +0300

    Exercise_4

---
+++ Lesson_3/Exercise_4/Exercise_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите элементы массива через пробел");
+            string s = Console.ReadLine();
+            Console.WriteLine("Введите требуемую сумму двух элементов массива");
+            int target = Convert.ToInt32(Console.ReadLine());
+            string[] inputStringArray = s.Split(' ');
+            int[] inputArray = new int[inputStringArray.Length];
+
+            for (int i = 0; i < inputStringArray.Length; i++)                
+                inputArray[i] = Convert.ToInt32(inputStringArray[i]);
+            Console.Write($"Индексы элементов массива, сумма которых равна {target}: ");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[j] + inputArray[i] == target)
+                    {
+                        Console.WriteLine($"{i} и {j} ");
+                    }
+                }
+            }
+            
+        }
+    }
+}
