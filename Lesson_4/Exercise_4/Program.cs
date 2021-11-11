61f92b16dddb2c6b1d6bfbda33d25c1e4646dd06
Nikita <nickita.sevsk@gmail.com>
2021-11-11 21:58:30 +0300

    Exercise_4

---
+++ Lesson_4/Exercise_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение большее, либо равное нулю");
+            var value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Число Фибоначчи для значения {value} равно {Fibo(value)}");
+            static int Fibo(int value)
+            {
+                if (value == 0)
+                    return 0;
+                if (value == 1 || value == 2)
+                    return 1;
+                return Fibo(value - 1) + Fibo(value - 2);
+            }
+        }
+    }
+}
