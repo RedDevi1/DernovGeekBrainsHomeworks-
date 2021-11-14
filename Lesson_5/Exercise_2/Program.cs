1999a6f518faa98b3e969616aad300a1f164bf1b
Nikita <nickita.sevsk@gmail.com>
2021-11-14 23:57:16 +0300

    Exercise_2

---
+++ Lesson_5/Exercise_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "startup.txt";
+            DateTime curDateTime = DateTime.Now;
+            var curTime = curDateTime.ToString().Split(' ');
+            File.AppendAllText(fileName, curTime[1]);
+        }
+    }
+}
