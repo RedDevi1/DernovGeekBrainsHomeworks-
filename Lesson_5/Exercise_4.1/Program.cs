98eb5d22800df48e9d4f4a46f21a3813e2cfc522
Nikita <nickita.sevsk@gmail.com>
2021-11-16 22:18:16 +0300

    Exercise_4_with_recursion

---
+++ Lesson_5/Exercise_4.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь, дерерво каталогов и файлов которого требуется сохранить");
+            var path = Console.ReadLine();
+            Console.WriteLine(Directory.Exists(path));
+            AppendWithRecursion(new DirectoryInfo(path));
+        }
+        static void AppendWithRecursion(DirectoryInfo root)
+        {
+            var filePath = "filepath.txt";
+            FileInfo[] files = root.GetFiles("*.*");
+            if (files != null)
+            {
+                foreach (FileInfo nameOfFile in files) //append all the file names to "filepath.txt"
+                {
+                    File.AppendAllText(filePath, nameOfFile.FullName);
+                    File.AppendAllText(filePath, Environment.NewLine);
+                }
+                DirectoryInfo[] subDir = root.GetDirectories(); //get all the subdirectories
+                foreach (DirectoryInfo curDir in subDir)
+                    AppendWithRecursion(curDir);
+            }
+        }
+    }
+}
