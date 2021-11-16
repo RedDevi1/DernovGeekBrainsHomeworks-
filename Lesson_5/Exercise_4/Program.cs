c74a9faa9ec34f2e2bde999fbc0925a4a72fb9cb
Nikita <nickita.sevsk@gmail.com>
2021-11-16 16:08:59 +0300

    Exercise_4_without_recursion

---
+++ Lesson_5/Exercise_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь, дерерво каталогов и файлов которого требуется сохранить");
+            var path = Console.ReadLine();
+            Console.WriteLine(Directory.Exists(path));
+            string[] entries = Directory.GetFileSystemEntries(path, "*", SearchOption.AllDirectories);
+            var filePath = "filepath.txt";
+            for (int i = 0; i < entries.Length; i++)
+            {
+                File.AppendAllText(filePath, entries[i]);
+                File.AppendAllText(filePath, Environment.NewLine);
+            }            
+        }
+    }
+}
